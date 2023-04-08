efe9044cdc0557605c80bdd11f212f03dfdbfd18
a-singh09 <a_singh09@yahoo.com>
2023-04-08 15:30:33 +0530

    first commit

---
+++ pages/mugs.js
@@ -0,0 +1,97 @@
+import React from 'react'
+import Link from 'next/link'
+
+const Mugs = () => {
+  return (
+    <div>
+      <section class="text-gray-400 bg-gray-900 body-font">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0410/0250/0260/products/BM-MV0017_1_1800x1800.jpg?v=1641279626" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">The Catalyzer</h2>
+                <p class="mt-1">$16.00</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0410/0250/0260/products/BM-MV0017_1_1800x1800.jpg?v=1641279626" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">Shooting Stars</h2>
+                <p class="mt-1">$21.15</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0410/0250/0260/products/BM-MV0017_1_1800x1800.jpg?v=1641279626" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">Neptune</h2>
+                <p class="mt-1">$12.00</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0410/0250/0260/products/BM-MV0017_1_1800x1800.jpg?v=1641279626" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">The 400 Blows</h2>
+                <p class="mt-1">$18.40</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0410/0250/0260/products/BM-MV0017_1_1800x1800.jpg?v=1641279626" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">The Catalyzer</h2>
+                <p class="mt-1">$16.00</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0410/0250/0260/products/BM-MV0017_1_1800x1800.jpg?v=1641279626" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">Shooting Stars</h2>
+                <p class="mt-1">$21.15</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0410/0250/0260/products/BM-MV0017_1_1800x1800.jpg?v=1641279626" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">Neptune</h2>
+                <p class="mt-1">$12.00</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://cdn.shopify.com/s/files/1/0410/0250/0260/products/BM-MV0017_1_1800x1800.jpg?v=1641279626" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">The 400 Blows</h2>
+                <p class="mt-1">$18.40</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
+
+export default Mugs
---
+++ pages/tshirts.js
@@ -0,0 +1,97 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Tshirts = () => {
+  return (
+    <>
+      <section class="text-gray-400 bg-gray-900 body-font">
+        <div class="container px-5 py-24 mx-auto">
+          <div class="flex flex-wrap -m-4">
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1625647770_7125040.jpg?format=webp&w=480&dpr=2.0" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">The Catalyzer</h2>
+                <p class="mt-1">$16.00</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1625647770_7125040.jpg?format=webp&w=480&dpr=2.0" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">Shooting Stars</h2>
+                <p class="mt-1">$21.15</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1625647770_7125040.jpg?format=webp&w=480&dpr=2.0" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">Neptune</h2>
+                <p class="mt-1">$12.00</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1625647770_7125040.jpg?format=webp&w=480&dpr=2.0" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">The 400 Blows</h2>
+                <p class="mt-1">$18.40</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1625647770_7125040.jpg?format=webp&w=480&dpr=2.0" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">The Catalyzer</h2>
+                <p class="mt-1">$16.00</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1625647770_7125040.jpg?format=webp&w=480&dpr=2.0" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">Shooting Stars</h2>
+                <p class="mt-1">$21.15</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1625647770_7125040.jpg?format=webp&w=480&dpr=2.0" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">Neptune</h2>
+                <p class="mt-1">$12.00</p>
+              </div>
+            </div>
+            <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
+              <Link href={'/products/wear-the-code'} class="block relative rounded overflow-hidden">
+                <img alt="ecommerce" class="object-cover object-center w-full h-full block" src="https://prod-img.thesouledstore.com/public/theSoul/uploads/catalog/product/1625647770_7125040.jpg?format=webp&w=480&dpr=2.0" />
+              </Link>
+              <div class="mt-4">
+                <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">CATEGORY</h3>
+                <h2 class="text-white title-font text-lg font-medium">The 400 Blows</h2>
+                <p class="mt-1">$18.40</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default Tshirts
